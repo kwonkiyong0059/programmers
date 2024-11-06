738445c8ceb13b1f8a0128e933594307ba67cad4
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-07 01:04:21 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,14 +1,10 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        String[] vowel = {"a", "e", "i", "o", "u"};
+        //String[] vowel = {"a", "e", "i", "o", "u"};
         
-        for (String v : vowel) {
-            my_string = my_string.replace(v, "");
-        }
-        
-        answer = my_string;
-        
+        answer= my_string.replaceAll("[aeiou]", "");
+             
         return answer;
     }
 }
