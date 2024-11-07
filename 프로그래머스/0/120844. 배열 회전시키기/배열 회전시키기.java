72889b29120c9a4aeecbfc1c36a983255bab3fa9
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-07 21:03:00 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.07 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        List<Integer> list = new ArrayList<>();
+        for(int num : numbers){
+            list.add(num);
+        }
+        
+        switch(direction){
+            case "right" :
+                Collections.rotate(list, 1);
+                break;
+            case "left" :
+                Collections.rotate(list, -1);
+                break;
+        }
+        
+        int[] answer = new int[numbers.length];
+        for(int i = 0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
