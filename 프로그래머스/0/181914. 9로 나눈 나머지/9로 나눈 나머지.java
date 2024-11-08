840444f444f07e4e42ec6b6fa58c53ea11d7f12c
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-09 01:53:53 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 4.82 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -3,14 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(String number) {
         int answer = 0;
-        int[] numArray = new int[number.length()];
         
-        for(int i = 0; i<number.length(); i++){
-            numArray[i] = Character.getNumericValue(number.charAt(i));
-            answer+=numArray[i];
+        for(int i =0; i<number.length(); i++){
+            answer += number.charAt(i) - '0';
         }
         
-        answer %= 9;
+        answer %=9;
         
         return answer;
         
