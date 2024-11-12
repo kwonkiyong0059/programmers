5e920eec00b44c151a5a35acccb4f6627fc23fa7
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-12 21:37:48 +0900

    [level 0] Title: 세로 읽기, Time: 18.84 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,17 +1,12 @@
 import java.util.*;
 class Solution {
     public String solution(String my_string, int m, int c) {
-        List<String> result = new ArrayList<>();
-        for(int i = 0; i<my_string.length(); i+=m){
-            String sub = my_string.substring(i, i+m);
-            result.add(sub);
-        }
-        
-        
         String answer = "";
-        for(String str : result){
-            answer += str.substring(c-1,c);
+        
+        for(int i = c-1; i<my_string.length(); i+=m){
+            answer += my_string.charAt(i);
         }
+        
         return answer;
     }
 }
