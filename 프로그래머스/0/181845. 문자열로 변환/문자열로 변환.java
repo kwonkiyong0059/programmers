c2031ded30d958b7b5c0fb29cdda0dccf7c204a3
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-13 19:51:50 +0900

    [level 0] Title: 문자열로 변환, Time: 0.04 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        answer = String.valueOf(n);
+        //answer = n +"";
+        //answer = Integer.toString(n);
+        
+        return answer;
+    }
+}
