002c8210cfa74dcf20d4c6d49b2ffe7b7e9b8b5a
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-15 14:45:58 +0900

    [level 0] Title: 배열 만들기 5, Time: 5.52 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,21 +1,18 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> lists = new ArrayList<>();
         
         for(int i=0; i<intStrs.length;i++){
             int x = Integer.parseInt(intStrs[i].substring(s, s + l));
             
             if(x>k){
-                list.add(x);
+                lists.add(x);
             }
         }
         
-        int[] answer = new int[list.size()];
-        for(int i = 0; i<list.size(); i++){
-            answer[i] = list.get(i);
-        }
         
-        return answer;
+        
+        return lists.toArray();
     }
 }
