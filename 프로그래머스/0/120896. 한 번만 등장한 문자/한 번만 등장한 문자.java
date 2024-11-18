e7eab20fc9aebb6db80b6341551dd17e24b00cc8
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-18 11:06:19 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.62 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        int[] frequency = new int[26];
+        int[] alp = new int[26];
         
         for(char c : s.toCharArray()){
-            frequency[c-'a']++;
+            alp[c-'a']++;
         }
         
         List<Character> unique = new ArrayList<>();
         for(char c : s.toCharArray()){
-            if(frequency[c-'a'] == 1){
+            if(alp[c-'a']==1){
                 unique.add(c);
             }
         }
@@ -18,7 +18,7 @@ class Solution {
         Collections.sort(unique);
         
         StringBuilder answer = new StringBuilder();
-        for(char c : unique){
+        for(char c: unique){
             answer.append(c);
         }
         
