7fd973f6f9d96c028f21b322a7393c4dbda72068
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-21 11:17:13 +0900

    [level 0] Title: qr code, Time: 0.13 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -2,9 +2,9 @@ class Solution {
     public String solution(int q, int r, String code) {
         StringBuilder answer = new StringBuilder();
         
-        for(int i=0; i<code.length(); i++){
-            if(i%q==r){
-                answer.append(code.charAt(i));
+        for (int i = 0; i < code.length(); i++) {
+            if (i % q == r) {
+                answer.append(code.charAt(i)); 
             }
         }
         
