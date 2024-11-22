1271fc1f673f6d98b3d9848fb86c83d14ce511c7
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-22 15:21:48 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.07 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -1,13 +1,14 @@
 import java.util.*;
 class Solution {
     public String[] solution(String my_str, int n) {
-        List<String> answer = new ArrayList<>();
+        List<String> str = new ArrayList<>();
         
-        for(int i=0; i<my_str.length();i+=n){
-            int end = Math.min(i+ n, my_str.length());
-            answer.add(my_str.substring(i, end));
+        for(int i= 0; i<my_str.length(); i+=n){
+            int end = Math.min(i+n, my_str.length());
+            str.add(my_str.substring(i, end));
+            
         }
         
-        return answer.toArray(new String[0]);
+        return str.toArray(new String[0]);
     }
 }
