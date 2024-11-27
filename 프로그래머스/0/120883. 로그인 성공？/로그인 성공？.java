cb413cce6d9a080d892b7baa16b12b1487fb6de3
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-27 15:42:23 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,17 +1,18 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        String answer = "";
+        String answer = "fail";
         
         for(int i=0; i<db.length;i++){
-            if(id_pw[0].equals(db[i][0]) && id_pw[1].equals(db[i][1])){
+            if(id_pw[0].equals(db[i][0])){
+                if(id_pw[1].equals(db[i][1])){
                 answer ="login";
                 break;
-            } else if(id_pw[0].equals(db[i][0])){
+                }
                 answer = "wrong pw";
                 break;
-            } else{
-                answer = "fail";
-            }
+            } 
+            
+            
         }
         
         return answer;
