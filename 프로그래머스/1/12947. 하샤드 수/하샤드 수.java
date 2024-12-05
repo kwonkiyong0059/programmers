3d6e1959c220e5e4ad59377efce2394e64385ff2
kwonkiyong0059 <kkoung425@gmail.com>
2024-12-05 12:26:16 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int y = x;
+        
+        int sum = 0;
+        while(y>0){
+            sum += y%10;
+            y/=10;
+        }
+        
+        
+        return x%sum==0? true:false;
+    }
+}
