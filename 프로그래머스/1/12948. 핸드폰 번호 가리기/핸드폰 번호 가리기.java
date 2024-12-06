a1167779548274eb862fc0da2bcf546b6cea975c
kwonkiyong0059 <kkoung425@gmail.com>
2024-12-06 10:47:41 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.40 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        String x="";
+        int idx = phone_number.length();
+        
+        x = phone_number.substring(idx-4);
+        
+        for(int i=0; i<idx-4;i++){
+            answer += "*";
+        }
+        
+        answer += x;
+        
+        return answer;
+    }
+}
