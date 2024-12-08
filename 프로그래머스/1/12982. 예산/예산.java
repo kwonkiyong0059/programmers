7a0c0d1a38d6bf982a54c5cefcd0b6dfb49a54ee
kwonkiyong0059 <kkoung425@gmail.com>
2024-12-08 23:42:30 +0900

    [level 1] Title: 예산, Time: 0.54 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        
+        for(int x : d){
+            if((budget-x) >= 0){
+                budget -= x;
+                answer++;
+            }else{
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
