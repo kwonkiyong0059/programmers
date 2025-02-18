c3a59f4d207cd97605da3dcc368b18d72c400eeb
kwonkiyong0059 <kkoung425@gmail.com>
2025-02-18 23:48:12 +0900

    [level 2] Title: 숫자의 표현, Time: 0.12 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,14 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
-        for(int i=1; i*(i-1)/2 < n; i++){
-            int numerator = n-(i*(i-1)/2);
-            if(numerator % i ==0){
-                answer++;
-            }
-        }
-        
-        return answer;
+   for (int i = 1; i <= n; i += 2) 
+       if (n % i == 0) 
+           answer++;
+
+   return answer;
     }
 }
